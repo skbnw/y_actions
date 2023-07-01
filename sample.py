2aafee1e060ac370017ad4bae40fe045fcf5df5e
skbnw <garden75@gmail.com>
2023-07-01 14:59:20 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -18,5 +18,9 @@ for file_name in file_list:
     if file_name.endswith(".xml"):
         source_path = os.path.join(source_directory, file_name)
         destination_path = os.path.join(destination_directory, file_name)
-        shutil.copy2(source_path, destination_path)
-        print(f"コピー完了: {file_name}")
+        try:
+            shutil.copy2(source_path, destination_path)
+            print(f"コピー完了: {file_name}")
+        except Exception as e:
+            print(f"コピー失敗: {file_name}")
+            print(f"エラーメッセージ: {str(e)}")
