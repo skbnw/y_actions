3e981d0410759e4488e025e25bb461577dc952ca
skbnw <garden75@gmail.com>
2023-07-01 14:49:51 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -17,6 +17,6 @@ file_list = os.listdir(source_directory)
 for file_name in file_list:
     if file_name.endswith(".xml"):
         source_path = os.path.join(source_directory, file_name)
-        destination_path = os.path.join(destination_directory, "copy_" + file_name)
-        shutil.copy(source_path, destination_path)
+        destination_path = os.path.join(destination_directory, file_name)
+        shutil.copy2(source_path, destination_path)
         print(f"コピー完了: {file_name}")
