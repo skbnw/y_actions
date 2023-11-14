7ef5f260bd0e706bf9346c2c585c0acefe7b0f8b
skbnw <garden75@gmail.com>
2023-11-14 10:09:55 +0900

    Update media_scraper_group_f_Ver.3.6.py

--- media_scraper_group_f_Ver.3.6.py
+++ media_scraper_group_f_Ver.3.6.py
@@ -75,7 +75,7 @@ for index, row in url_data.iterrows():
     df = pd.DataFrame(data, columns=["media_en", "media_jp", "title_articles", "link_articles", "date_original"])
 
     # 保存するディレクトリが存在しない場合は作成する
-    folder_name = now.strftime("html_mediaALL_a_%Y%m_%W")
+    folder_name = now.strftime("html_mediaALL_f_%Y%m_%W")
     os.makedirs(folder_name, exist_ok=True)
 
     # ファイル名を設定
